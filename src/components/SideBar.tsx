533c6ac9c6f288812f358d02690eae0e3bb5ecbe
Separatehhh23 <separatehhh23@gmail.com>
2023-06-02 19:07:45 +0200

    Fixed error in SideBar.tsx

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,32 +1,33 @@
 import { BsPlus, BsFillLightningFill, BsGearFill } from 'react-icons/bs';
 import { FaFire, FaPoo } from 'react-icons/fa';
+import { ReactNode } from 'react';
 
-
-const SideBar = () => {
-
-
-    return (
-        <div className="fixed top-0 left-0 h-screen w-16 m-0
-                        flex flex-col
-                        bg-gray-100 text-gray-900 shadow-lg
-                        dark:bg-gray-900 dark:text-white">
-                            
-            <SideBarIcon icon={<FaFire size="28" />} />
-            <SideBarIcon icon={<BsPlus size="28" />} />
-            <SideBarIcon icon={<BsFillLightningFill size="28" />} />
-            <SideBarIcon icon={<FaPoo size="28" />} />
-        </div>
-    );
+type SideBarIconProps = {
+  icon: ReactNode;
+  text?: string;
 };
 
-const SideBarIcon = ({ icon, text = 'tooltip 💡' }) => (
-    <div className="sidebar-icon group">
-        {icon}
-
-        <span className="sidebar-tooltip group-hover:scale-100">
-            {text}
-        </span>
+const SideBar = () => {
+  return (
+    <div className="fixed top-0 left-0 h-screen w-16 m-0
+                    flex flex-col
+                    bg-gray-100 text-gray-900 shadow-lg
+                    dark:bg-gray-900 dark:text-white">
+      <SideBarIcon icon={<FaFire size="28" />} text="Fire" />
+      <SideBarIcon icon={<BsPlus size="28" />} text="Plus" />
+      <SideBarIcon icon={<BsFillLightningFill size="28" />} text="Lightning" />
+      <SideBarIcon icon={<FaPoo size="28" />} text="Poo" />
     </div>
+  );
+};
+
+const SideBarIcon = ({ icon, text }: SideBarIconProps) => (
+  <div className="sidebar-icon group">
+    {icon}
+    <span className="sidebar-tooltip group-hover:scale-100">
+      {text}
+    </span>
+  </div>
 );
 
 export default SideBar;
