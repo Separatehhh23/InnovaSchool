05c92108a24ac83794ddd0b56272d23dd00fa134
Separatehhh23 <separatehhh23@gmail.com>
2023-06-02 17:48:25 +0200

    First commit

---
+++ src/components/SideBar.tsx
@@ -0,0 +1,32 @@
+import { BsPlus, BsFillLightningFill, BsGearFill } from 'react-icons/bs';
+import { FaFire, FaPoo } from 'react-icons/fa';
+
+
+const SideBar = () => {
+
+
+    return (
+        <div className="fixed top-0 left-0 h-screen w-16 m-0
+                        flex flex-col
+                        bg-gray-100 text-gray-900 shadow-lg
+                        dark:bg-gray-900 dark:text-white">
+                            
+            <SideBarIcon icon={<FaFire size="28" />} />
+            <SideBarIcon icon={<BsPlus size="28" />} />
+            <SideBarIcon icon={<BsFillLightningFill size="28" />} />
+            <SideBarIcon icon={<FaPoo size="28" />} />
+        </div>
+    );
+};
+
+const SideBarIcon = ({ icon, text = 'tooltip 💡' }) => (
+    <div className="sidebar-icon group">
+        {icon}
+
+        <span className="sidebar-tooltip group-hover:scale-100">
+            {text}
+        </span>
+    </div>
+);
+
+export default SideBar;
