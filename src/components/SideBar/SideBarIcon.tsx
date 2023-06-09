684c0d1df505b1f3f7242562df2f2adb58b9e987
Separatehhh23 <separatehhh23@gmail.com>
2023-06-09 19:54:48 +0200

    Moved SideBarIcon component to separate file

---
+++ src/components/SideBar/SideBarIcon.tsx
@@ -0,0 +1,21 @@
+import { ReactNode } from "react";
+
+type SideBarIconProps = {
+    icon: ReactNode;
+    text?: string;
+    link?: string;
+    onClick?: () => void;
+};
+
+const SideBarIcon = ({ icon, text, link, onClick }: SideBarIconProps) => (
+    <a href={link} onClick={onClick}>
+      <div className="sidebar-icon group">
+        {icon}
+        <span className="sidebar-tooltip group-hover:scale-100">
+          {text}
+        </span>
+      </div>
+    </a>
+);
+
+export default SideBarIcon;
